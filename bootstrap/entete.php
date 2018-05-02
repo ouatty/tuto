b6dd6b7e92a118dbe4314c1ac402fb14318c67b7
ouatty <38909925+ouatty@users.noreply.github.com>
2018-05-02 12:40:10 +0200

    Add files via upload

---
+++ bootstrap/entete.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bootstrap</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style-form.css">
+
+</head>
+<body>
+
+
+<section id="header">
+	<div class="container">
+		<nav class="navbar navbar-default">
+  			<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+   				 <div class="navbar-header">
+			    	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				        
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+			     	 </button>
+			      	<a class="navbar-brand" href="./index.html">DUNAMIS</a>
+			   	 </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    
+			      <ul class="nav navbar-nav navbar-right">
+			        <li><a href="#">HOME</a></li>
+			        <li><a href="#">SERVICE</a></li>
+			        <li><a href="#">FORMATION</a></li>
+			        <li><a href="#">MARKETING</a></li>
+			        <li><a href="form-message.php">CONTACT</a></li>
+			       
+			      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
+</section>
+</body>
---
+++ bootstrap/form-message.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formuaire</title>
+	<link rel="stylesheet" type="text/css" href="style-form.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<section>	
+<div class="container-fluid">	
+
+	<div  class="	container">	
+
+		<div class="formBox">	
+			<div class="form">	
+				<form>	
+					<div class="row">	
+						<div class="col-sm-12">
+							<h1> Contact Us </h1>
+						</div>
+					</div>
+					<div class="row">
+							<div class="col-sm-6">
+								<div class="inputBox">
+										<div class="inputText fokkus">NOM  </div>
+										<input type="text" name="" class="input">
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="inputBox ">
+										<div class="inputText">PRENOM  </div>
+										<input type="text" name="" class="input">
+								</div>
+							</div>
+					</div>
+					<div class="row">
+							<div class="col-sm-6">
+								<div class="inputBox ">
+										<div class="inputText">EMAIL </div>
+										<input type="text" name="" class="input">
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="inputBox ">
+										<div class="inputText">MOBILE </div>
+										<input type="text" name="" class="input">
+								</div>
+							</div>
+					</div>
+					<div class="row">
+							<div class="col-sm-12">
+								<div class="inputBox ">
+										<div class="inputText">MESSAGE </div>
+										<textarea class="input">	</textarea>
+								</div>
+							</div>
+							
+					</div>
+					<div class="row">
+							<div class="col-sm-12">
+								<div class="inputBox">
+										<input type="submit" name="" value="Envoyer Message" class="button">
+										
+								</div>
+							</div>
+							
+					</div>
+				</form>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+</section>
+
+
+</body>
+</html>
